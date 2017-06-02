d033dd69e0cc3b1fbb2f813c33c8d23e9ca7cdea
edharcourt@gmail.com <edharcourt@gmail.com>
2017-06-02 12:48:03 -0400

    update exp growth

--- python-scipy/exponential_growth.py
+++ python-scipy/exponential_growth.py
@@ -9,3 +9,21 @@ plt.plot(points)
 plt.ylabel('94^n')
 plt.xlabel('i')
 plt.show()
+
+i = 1
+sum = 0
+while i <= 10:
+    sum = sum + 94**i
+    i = i + 1
+
+print(sum)
+
+i = 1
+sum = 0
+pow94 = 94
+while i <= 10:
+    sum = sum + pow94
+    pow94 = pow94 * 94
+    i = i + 1
+
+print(sum)
