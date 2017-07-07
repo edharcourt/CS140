52faa16181cc4ad81adaeb95f75a2b43bdf8b827
Ed Harcourt <edharcourt@stlawu.edu>
2017-07-07 10:06:02 -0400

    more particle sim stuff

--- python/particle_v1.py
+++ python/particle_v1.py
@@ -2,10 +2,46 @@ import pygame, color
 pygame.init()
 side = 600
 win = pygame.display.set_mode((side,side))
+width = win.get_width()
+height = win.get_height()
 
 ball = pygame.image.load("../images/ball.png").convert_alpha()
-win.fill(color.black)
-win.blit(ball, (side//2, side//2))
+ball_w = ball.get_width()
+ball_h = ball.get_height()
+ball_x = width//2 - ball_w//2
+ball_y = height//2 - ball_h//2
+ball_dx = .04
+ball_dy = .01
+
+win.fill(color.lightgray)
+win.blit(ball, (ball_x, ball_y))
 pygame.display.update()
+
+def move(x, y, dx, dy):
+
+    x = x + dx
+    y = y + dy
+
+    if x < 0:
+        x = 0
+        dx = -dx
+    elif x + ball_w >= width:
+        x = width - ball_w
+        dx = -dx
+
+    if y < 0:
+        y = 0
+        dy = -dy
+    elif y +ball_h >= height:
+        y = height - ball_h
+        dy = -dy
+
+    return (x,y,dx,dy)
+
+while True:
+    win.fill(color.lightgray)
+    (ball_x,ball_y,ball_dx,ball_dy) = move(ball_x,ball_y,ball_dx,ball_dy)
+    win.blit(ball, (ball_x, ball_y))
+    pygame.display.update()
 
 input()
