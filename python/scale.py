d139445bc58d4ab9e46cb58ade8afd8632333c78
Ed Harcourt <edharcourt@stlawu.edu>
2017-07-22 22:57:42 -0400

    scale

--- python/scale.py
+++ python/scale.py
@@ -1,5 +1,3 @@
-import pygame, util,color
-pygame.init()
 
 #
 # A function that returns a new scaled image from an
@@ -22,15 +20,26 @@ def scale(original, scaled_w, scaled_h):
     return scaled
 
 # M A I N
-win = pygame.display.set_mode((600,600))
+import pygame, util,color
+pygame.init()
+
 image = pygame.image.load(
     "../images/bug.jpg")
 
 # call our new scale function
-scaled_image = scale(image, image.get_width()//4,
-                     image.get_height()//4)
+scaled_image = scale(image, image.get_width()//3,
+                     image.get_height()//3)
 
+win = pygame.display.set_mode((image.get_width(), image.get_height()))
+win.blit(image, (0,0))
 
+win.blit(scaled_image, (win.get_width()//2 - scaled_image.get_width()//2,
+                        win.get_height()//2 - scaled_image.get_height()//2))
+
+pygame.display.update()
+util.wait_for_click()
+
+'''
 # slide image in from left
 for i in range(win.get_width() - scaled_image.get_width()):
     win.fill(color.white)
@@ -40,6 +49,6 @@ for i in range(win.get_width() - scaled_image.get_width()):
                  scaled_image.get_height()))
     pygame.display.update()
     pygame.time.delay(10)
+'''
+#pygame.image.save(win, "D:/Shared/images/big_in_a_bug.png")
 
-pygame.display.update()
-util.wait_for_click()
