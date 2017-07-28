af8d29c43ec94b69be70aebcb3f3e7e0939e5171
Ed Harcourt <edharcourt@stlawu.edu>
2017-07-28 10:11:16 -0400

    weighted rgb

--- python/grayscale.py
+++ python/grayscale.py
@@ -8,10 +8,14 @@ image = image.convert_alpha()
 for y in range(image.get_height()):
     for x in range(image.get_width()):
         (r, g, b, a) = image.get_at((x, y))
-        gray = (r + g + b) // 3
+        #gray = (r + g + b) // 3
+        r = .3*r
+        g = .59*g
+        b = .11*b
+        gray = int(r + g + b)
         image.set_at((x, y), (gray, gray, gray))
 
 win.blit(image, (0,0))
 pygame.display.update()
-pygame.image.save(image, "D:/Shared/images/gray_bug.jpg")
+pygame.image.save(image, "D:/Shared/images/gray_weighted_rgb_bug.jpg")
 util.wait_for_click()
