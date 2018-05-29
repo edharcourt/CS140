1142e1a0055e60a11c8b9239ab6961593ce4edd0
Ed Harcourt <edharcourt@stlawu.edu>
2018-05-28 23:07:28 -0400

    finish up caesar cipher

--- python/scratch.py
+++ python/scratch.py
@@ -1,5 +1,7 @@
-
-3*3   # pointless
-
-s = int(input("Enter a number: "))
-print("1000 divided by", s, "is", 1000/s)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+print(alphabet.find('a'))
+print(alphabet.find('z'))
+print(alphabet.find('m'))
+print(alphabet[0], alphabet[25], alphabet[12])
+print(alphabet[alphabet.find('x')])
+print(alphabet.find(alphabet[23]))
