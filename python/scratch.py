932314d1864f3c3c2624f9d3e0f433e96e5656d6
Ed Harcourt <edharcourt@stlawu.edu>
2018-06-03 23:11:25 -0400

    fix typos

--- python/scratch.py
+++ python/scratch.py
@@ -1,7 +1,7 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-print(alphabet.find('a'))
-print(alphabet.find('z'))
-print(alphabet.find('m'))
-print(alphabet[0], alphabet[25], alphabet[12])
-print(alphabet[alphabet.find('x')])
-print(alphabet.find(alphabet[23]))
+ alphabet = "abcdefghijklmnopqrstuvwxyz"
+ print(alphabet.find('a'))
+ print(alphabet.find('z'))
+ print(alphabet.find('m'))
+ print(alphabet[0], alphabet[25], alphabet[12])
+ print(alphabet[alphabet.find('x')])
+ print(alphabet.find(alphabet[23]))
