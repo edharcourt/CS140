3fedec49bfca0563be0a04f34ac08563c930245d
Ed Harcourt <edharcourt@stlawu.edu>
2018-06-10 23:14:15 -0400

    .

--- python/retirement.py
+++ python/retirement.py
@@ -1,6 +1,6 @@
 
 investments = 798000
-ror = .04
+ror = .03
 salary = 100000
 invest_per_year = .21
 SLU_match = .10
@@ -12,25 +12,37 @@ print("age", '\t', "Salary", '\t', "Investments")
 print("----------------------------------------")
 
 for i in range(age,retire_age + 1):
-    investments = investments * (1 + ror)
-    investments = investments + salary*invest_per_year + salary*SLU_match
-    salary = salary * (1 + salary_increase_per_year)
     print(i, '\t\t', int(salary), '\t\t', int(investments))
+
+    # investment return
+    investments = investments * (1 + ror)
+
+    # add in new contributions
+    investments = investments + salary*invest_per_year + salary*SLU_match
+
+    # update new salary
+    salary = salary * (1 + salary_increase_per_year)
 
 
 # retirement spending
 
-spend_rate = .04
-inflation = .02
-age = 67
-spend = investments * spend_rate
+spend_rate = .04  # spend 4% per year
+inflation = .02   # rate of inflation
+age = 67          # retirement age
+spend = investments * spend_rate   # initial amount to spend
 
 print('\n\n', 'age', '\t', 'Spend', '\t\t', 'Investments')
 print('-------------------------------------------------------------------')
 while investments > 0:
     print(age, '\t\t', int(spend), '\t\t', int(investments))
+
+    # portfolio after yearly withdrawal
     investments = investments - spend
+
+    # portfolio should still be gaining interest
     investments = investments * (1.02)
+
+    # need to increase spending by rate of inflation
     spend = spend * (1 + inflation)
     age = age + 1
 
