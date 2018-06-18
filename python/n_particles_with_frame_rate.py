a98ba524021af0d6e22b3e1538cd552f31913364
Ed Harcourt <edharcourt@stlawu.edu>
2018-06-17 22:45:30 -0400

    .

--- python/n_particles_with_frame_rate.py
+++ python/n_particles_with_frame_rate.py
@@ -1,4 +1,4 @@
-import pygame, color, random
+import pygame, color, random, math
 pygame.init()
 side = 600
 win = pygame.display.set_mode((side,side))
@@ -20,8 +20,8 @@ for row in range(r, side, 6*r):
         y.append(row)
 
         # generate random velocities
-        tdx = side / (random.random() * 2 + 2)
-        tdy = side / (random.random() * 2 + 2)
+        tdx = side / (random.random() * 4 + 4)
+        tdy = side / (random.random() * 4 + 4)
         if random.random() < .5:
             tdx = -tdx
 
@@ -31,12 +31,16 @@ for row in range(r, side, 6*r):
         dx.append(tdx)
         dy.append(tdy)
 
+"""
 win.fill(color.lightgray)
 for i in range(len(x)):
     win.blit(ball, (x[i], y[i]))
 pygame.display.update()
-input()
+"""
 
+# compute the distance between two particles
+def distance(x1,y1,x2,y2):
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
 def move(x, y, dx, dy):
 
@@ -69,6 +73,12 @@ while True:
     for i in range(len(x)):
         (x[i],y[i],dx[i],dy[i]) = move(x[i],y[i],dx[i],dy[i])
         win.blit(ball, (x[i], y[i]))
+
+    for i in range(len(x) - 1):
+        for j in range(i+1, len(x)):
+            if distance(x[i] + r, y[i] + r, x[j] + r, y[j] + r) < 2 * r:
+                (dx[i], dx[j]) = (dx[j], dx[i])
+                (dy[i], dy[j]) = (dy[j], dy[i])
 
     pygame.display.update()
     pygame.event.poll()
