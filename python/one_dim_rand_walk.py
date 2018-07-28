a7f84333c955f09a2d43b030fc91525a3bb5403f
Ed Harcourt <edharcourt@gmail.com>
2018-07-28 15:56:09 -0400

    .

--- python/one_dim_rand_walk.py
+++ python/one_dim_rand_walk.py
@@ -8,6 +8,9 @@ cash = 0
 win.fill(color.white)
 scale = 100
 
+low = cash
+high = cash
+
 while i < h*scale:
     n = random.randrange(2)
 
@@ -18,8 +21,18 @@ while i < h*scale:
 
     i = i + 1
 
+    if cash > high:
+        high = cash
+        pygame.draw.circle(win, color.green, (i//scale, h//2-cash), 2)
+
+    elif cash < low:
+        low = cash
+        pygame.draw.circle(win, color.red, (i//scale, h//2-cash), 2)
+
     # plot cash at time i//scale
     win.set_at((i//scale, h//2-cash), color.black)
 
     pygame.draw.line(win, color.red, (0,h//2), (h-1,h//2))
     pygame.display.update()
+
+pygame.image.save(win, "hi-low-1d-rand-walk.png")
