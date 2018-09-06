5bc79176e270032046eb6a52f89b1bc97a33d076
edharcourt@gmail.com <edharcourt@gmail.com>
2018-09-06 13:56:48 -0400

    .

--- python/haha.py
+++ python/haha.py
@@ -9,11 +9,10 @@ pygame.draw.circle(win, color.yellow, (200,200), 200)
 pygame.draw.arc(win, color.black, (50,50,300,300), math.pi, 2*math.pi, 150)
 
 pygame.draw.ellipse(win, color.red, (125,275,150,75))
-pygame.draw.line(win, color.black, (80,120), (160,140),5)
-pygame.draw.line(win, color.black, (80,160), (160,140),5)
-pygame.draw.line(win, color.black, (400-80,120), (400-160,140),5)
-pygame.draw.line(win, color.black, (400-80,160), (400-160,140),5)
-
+pygame.draw.line(win, color.black, (70,120), (160,140),5)
+pygame.draw.line(win, color.black, (60,160), (160,140),5)
+pygame.draw.line(win, color.black, (400-70,120), (400-160,140),5)
+pygame.draw.line(win, color.black, (400-60,160), (400-160,140),5)
 
 pygame.display.update()
 input()
