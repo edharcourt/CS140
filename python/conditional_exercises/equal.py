7bd4ebe6777b9079bbb2e4aa0385c9324958db27
Ed Harcourt <edharcourt@stlawu.edu>
2018-09-24 22:44:53 -0400

    .

---
+++ python/conditional_exercises/equal.py
@@ -0,0 +1,9 @@
+
+x = int(input("Enter number: "))
+y = int(input("Enter number: "))
+z = int(input("Enter number: "))
+
+if x == y and y == z:
+    print('equal')
+else:
+    print('not equal')
---
+++ python/conditional_exercises/sort3.py
@@ -0,0 +1,8 @@
+
+x = int(input("Enter number: "))
+y = int(input("Enter number: "))
+z = int(input("Enter number: "))
+
+# insert code here to 
+
+print(x,y,z)
