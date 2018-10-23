6fbcdcbbf9139f345843ce16ff51cfeb4c5c5cc4
Ed Harcourt <edharcourt@stlawu.edu>
2018-10-22 21:00:55 -0400

    .

--- python/loop_exercises/forloops.py
+++ python/loop_exercises/forloops.py
@@ -10,3 +10,14 @@ for i in range(8,3,-1):
 
 for i in range(3,8):
     print(i,end="")
+
+print()
+print('i','\t','j')
+print('-','\t','-')
+for i in range(4):
+    for j in range(3):
+        print(i,'\t',j)
+print('=============')
+for i in range(4):
+    for j in range(i,3):
+        print(i,'\t',j)
